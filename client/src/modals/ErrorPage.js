f6b816e5ef84bd2ba5f33ff304b7b4c521c4b89a
AzaniaBG <azaniazbaker@gmail.com>
2020-05-25 19:25:45 -0700

    changed paths

--- client/src/modals/ErrorPage.js
+++ client/src/modals/ErrorPage.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom';
 export default function(props) {
     return (
         <section id="ErrorPage-container">
-            <Route exact path="https://azaniabg.github.io/Six-Degrees/error">
+            <Route exact path="/error">
             <h3>Heck! We haz error. </h3>
             </Route>
         </section>
